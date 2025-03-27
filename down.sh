9693dac008e61c35fe7fa57bb35c3c59fd46b8c2
Luca Marchiori <luca.marchiori@hotmail.it>
2025-03-27 15:55:17 +0100

    Added Minikube tunnel cleanup on down script

--- down.sh
+++ down.sh
@@ -7,3 +7,5 @@ helm uninstall --namespace edgehog-dev ingress-nginx
 kubectl delete all --all -n edgehog-dev
 kubectl delete pvc --all -n edgehog-dev
 kubectl delete secrets --all -n edgehog-dev
+
+minikube tunnel -c
